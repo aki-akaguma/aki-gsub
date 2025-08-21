fc7f4bb175a5aa84738a2ab685673c2a748ceec8
aki <aki.akaguma@hotmail.com>
2025-08-21 16:26:04 +0900

    refactored lib.rs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Changed
 * updated: runnel(0.4.0)
 * updated: rust-version-info-file(0.2.0)
+* refactored `lib.rs`
 
 ### Fixed
 * minimum support version in doc
--- src/run.rs
+++ src/run.rs
@@ -28,7 +28,57 @@ struct ReplacedOut {
     out_s: String,
 }
 
-fn format(line_offset: usize, caps: &Captures<'_>, fmt: &str) -> ReplacedOut {
+fn do_match_proc(
+    sioe: &RunnelIoe,
+    conf: &CmdOptConf,
+    env: &EnvConf,
+    regfmts: &[RegexAndFormat],
+) -> anyhow::Result<()> {
+    for line in sioe.pg_in().lines() {
+        let line_s = line?;
+        let line_ss = line_s.as_str();
+        //
+        let routs: Vec<ReplacedOut> = make_replaced_out_vec(regfmts, line_ss)?;
+        if !routs.is_empty() {
+            let out_s = make_out_s(conf, env, line_ss, routs)?;
+            sioe.pg_out().write_line(out_s)?;
+        } else if !conf.flg_quiet {
+            sioe.pg_out().write_line(line_s)?;
+        }
+    }
+    sioe.pg_out().flush_line()?;
+    //
+    Ok(())
+}
+
+fn make_replaced_out_vec(
+    regfmts: &[RegexAndFormat],
+    line_ss: &str,
+) -> anyhow::Result<Vec<ReplacedOut>> {
+    let line_len: usize = line_ss.len();
+    let mut routs: Vec<ReplacedOut> = Vec::new();
+    //
+    for regfmt in regfmts {
+        let fmt = &regfmt.format;
+        let re = &regfmt.regex;
+        let mut st: usize = 0;
+        while let Some(caps) = re.captures(&line_ss[st..]) {
+            let mat = match caps.get(0) {
+                Some(mat) => mat,
+                None => break,
+            };
+            let rep_out = make_replaced_out_one(st, &caps, fmt);
+            routs.push(rep_out);
+            st += mat.end();
+            if st >= line_len {
+                break;
+            }
+        }
+    }
+    Ok(routs)
+}
+
+fn make_replaced_out_one(line_offset: usize, caps: &Captures<'_>, fmt: &str) -> ReplacedOut {
     let mut v_out_s = String::new();
     let mat = match caps.get(0) {
         Some(mat) => mat,
@@ -84,81 +134,42 @@ fn format(line_offset: usize, caps: &Captures<'_>, fmt: &str) -> ReplacedOut {
     }
 }
 
-fn do_match_proc(
-    sioe: &RunnelIoe,
+fn make_out_s(
     conf: &CmdOptConf,
     env: &EnvConf,
-    regfmts: &[RegexAndFormat],
-) -> anyhow::Result<()> {
+    line_ss: &str,
+    mut routs: Vec<ReplacedOut>,
+) -> anyhow::Result<String> {
     let color_start_s = env.color_seq_start.as_str();
     let color_end_s = env.color_seq_end.as_str();
     let color_is_alyways = matches!(conf.opt_color, OptColorWhen::Always);
+    let line_len: usize = line_ss.len();
     //
-    for line in sioe.pg_in().lines() {
-        let line_s = line?;
-        let line_ss = line_s.as_str();
-        let line_len: usize = line_ss.len();
-        //
-        //
-        let mut routs: Vec<ReplacedOut> = Vec::new();
-        //
-        for regfmt in regfmts {
-            let fmt = &regfmt.format;
-            let re = &regfmt.regex;
-            let mut st: usize = 0;
-            while let Some(caps) = re.captures(&line_ss[st..]) {
-                let mat = match caps.get(0) {
-                    Some(mat) => mat,
-                    None => break,
-                };
-                let rep_out = format(st, &caps, fmt);
-                routs.push(rep_out);
-                st += mat.end();
-                if st >= line_len {
-                    break;
-                }
-            }
+    routs.sort_unstable_by(|a, b| a.st.cmp(&b.st));
+    //
+    let mut out_s: String = String::new();
+    let mut prev_ed: usize = 0;
+    for rout in &routs {
+        let next_st = rout.st;
+        if prev_ed < next_st {
+            out_s.push_str(&line_ss[prev_ed..next_st]);
         }
-        if !routs.is_empty() {
-            //
-            routs.sort_unstable_by(|a, b| a.st.cmp(&b.st));
-            //
-            let mut out_s: String = String::new();
-            let mut prev_ed: usize = 0;
-            for rout in &routs {
-                let next_st = rout.st;
-                if prev_ed < next_st {
-                    out_s.push_str(&line_ss[prev_ed..next_st]);
-                }
-                if next_st >= line_len {
-                    prev_ed = rout.ed;
-                    break;
-                }
-                if color_is_alyways {
-                    out_s.push_str(color_start_s);
-                }
-                out_s.push_str(rout.out_s.as_str());
-                if color_is_alyways {
-                    out_s.push_str(color_end_s);
-                }
-                //
-                prev_ed = rout.ed;
-            }
-            if prev_ed < line_len {
-                out_s.push_str(&line_ss[prev_ed..]);
-            }
-            //
-            sioe.pg_out().write_line(out_s)?;
-            //#[rustfmt::skip]
-            //sioe.pg_out().lock().write_fmt(format_args!("{out_s}\n"))?;
-        } else if !conf.flg_quiet {
-            sioe.pg_out().write_line(line_s)?;
-            //#[rustfmt::skip]
-            //sioe.pg_out().lock().write_fmt(format_args!("{line_ss}\n"))?;
+        if next_st >= line_len {
+            prev_ed = rout.ed;
+            break;
         }
+        if color_is_alyways {
+            out_s.push_str(color_start_s);
+        }
+        out_s.push_str(rout.out_s.as_str());
+        if color_is_alyways {
+            out_s.push_str(color_end_s);
+        }
+        //
+        prev_ed = rout.ed;
     }
-    //
-    sioe.pg_out().flush_line()?;
-    //
-    Ok(())
+    if prev_ed < line_len {
+        out_s.push_str(&line_ss[prev_ed..]);
+    }
+    Ok(out_s)
 }
