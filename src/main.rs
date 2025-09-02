949c0d75d5dcaaed1aef1f18539e4d844412c3ff
aki <aki.akaguma@hotmail.com>
2025-09-02 22:50:06 +0900

    refactor main()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use libaki_gsub::execute;
 use runnel::RunnelIoeBuilder;
-use std::io::Write;
 
 fn main() {
     // fast mem operation.
@@ -10,19 +9,12 @@ fn main() {
     let mut env_args: Vec<String> = std::env::args().collect();
     let _program = env_args.remove(0);
     let program = env!("CARGO_PKG_NAME");
-    let env_args: Vec<&str> = env_args.iter().map(std::string::String::as_str).collect();
+    let env_args: Vec<&str> = env_args.iter().map(String::as_str).collect();
     //
     let sioe = RunnelIoeBuilder::new().build();
     //
-    match execute(&sioe, program, &env_args) {
-        Ok(_) => {}
-        Err(err) => {
-            #[rustfmt::skip]
-            let _ = sioe.pg_err().lock()
-                .write_fmt(format_args!("{program}: {err:#}\n"));
-            std::process::exit(1);
-        }
+    if let Err(err) = execute(&sioe, program, &env_args) {
+        let _ = sioe.pg_err().write_line(format!("{program}: {err:#}"));
+        std::process::exit(1);
     };
-    //
-    std::process::exit(0);
 }
